58c442b6b076abd188658eebfdcb83bed97a7420
Francois <francois.gentilleau@gmail.com>
2026-06-02 13:42:22 +1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-">salut</h1>
+      <h2 className='text-blue-600 text-lg h-3 w-9 bg-slate-500'  > hello</h2>
+    
+    </div>
+  );
+};
+
+export default App;
